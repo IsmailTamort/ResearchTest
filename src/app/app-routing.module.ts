f0fa99080026e7e906b173d18f1163ed0f4835e5
IsmailTamort <i.tamort@gmail.com>
2022-11-21 23:11:10 +0100

    ba3di

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SearchComponent } from './search/search.component';
+import { ViewComponent } from './view/view.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'search',component: SearchComponent},
+  {path:'view/:id',component: ViewComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
